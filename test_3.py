edce31f5d73a508811e10b471372477c488e07dd
Zemlyanoy <146558173+Zemlyanoy@users.noreply.github.com>
2024-01-15 13:48:16 +0000

    test_3

--- test_3.py
+++ test_3.py
@@ -8,20 +8,20 @@ anim_object, = plt.plot([], [], '-', lw=2) # Объект анимации
  
 xdata, ydata = [], [] # Координаты объекта анимации
  
-ax.set_xlim(0, 2*np.pi) # Пределы изменения переменной Х
-ax.set_ylim(-1, 1) # Пределы изменения переменной У
+ax.set_xlim(-20, 20) # Пределы изменения переменной Х
+ax.set_ylim(-20, 20) # Пределы изменения переменной У
  
 # Функция подстановки координат в объект анимации
 def update(frame):
-    xdata.append(frame) # Рассщет координаты Х
-    ydata.append(np.sin(frame)) # Рассщет координаты У
+    xdata.append(16 * np.sin(frame) ** 3) # Рассщет координаты Х
+    ydata.append(13 * np.cos(frame) - 5 * np.cos(frame * 2) - 2 * np.cos(frame * 3) - np.cos(frame * 4)) # Рассщет координаты У
     anim_object.set_data(xdata, ydata) # Передача координат
     return anim_object,
  
 ani = FuncAnimation(fig, # Стандартный вызов пространства
                     update, # Вызов функции подстановки координат
                     frames=np.arange(0, 2*np.pi, 0.1),
-                    interval=100 # Интервал между кадрами,
+                    interval=15 # Интервал между кадрами,
                     )            # по умолчанию 200 милисекунд
 	
-ani.save('animation_1.gif')
+ani.save('<3.gif')

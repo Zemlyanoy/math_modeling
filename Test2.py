f1a3028330de768b4e862237781960529b521e34
Zemlyanoy <146558173+Zemlyanoy@users.noreply.github.com>
2024-01-20 15:59:11 +0000

    Test4

--- Test2.py
+++ Test2.py
@@ -2,15 +2,18 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 
+z = int(input())
+a = z * np.pi / 4
  
 def animate(i):
-    ball.set_data(circle_move(Vx0 = 0.005, time=i, R = 4))
+    ball.set_data(circle_move(Vy0 = 0.005, Vx0 = 0.005, time=i, R = 4, alpha = a))
 
 
-def circle_move(R, Vx0, time):
+def circle_move(R, Vx0, time, Vy0, alpha):
+    y0 = Vy0 * np.sin(alpha)
     x0 = Vx0 * time
     x = x0 * R
-    y = 0
+    y = y0
     return x, y
 
 
@@ -29,6 +32,11 @@ if __name__ == '__main__':
                         frames=180,
                         interval=30)
  
+    plt.plot([-100, 100], [0, 0])
+    plt.plot([-100, 100], [2, 2])
+    plt.grid()
+
+
     ani.save('anime.gif') 
 
 

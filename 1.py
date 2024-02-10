afd745c8c532183a58a4fe6ced505cab9bd63042
Zemlyanoy <146558173+Zemlyanoy@users.noreply.github.com>
2024-02-10 15:04:36 +0000

    3

--- 1.py
+++ 1.py
@@ -9,7 +9,7 @@ def radio_function(m, t):
     return dmdt
 
 m_0 = 10
-k = 1.61 * 10**(-6) 
+k = 1.61 * 10**(-6) # Постоянная распада для Висмута 210
  
 '''
 k_Ur_238 = 4.84*10**(-18) # Уран 238
@@ -18,9 +18,6 @@ k_Tl_210 = 8.76*10**(-3) # Талий 210
 m_t = odeint(radio_function, m_0, t)
 
 plt.plot(t, m_t[:,0], label='Распад Висмута 210')
-plt.xlabel('Период распада, секунды')
-plt.ylabel('Функция распада')
-plt.title('Радиоактивный распад')
-plt.legend()
+
  
-plt.show()
+plt.savefig('fig_png')
